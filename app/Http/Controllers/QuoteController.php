669a0ab91b849d2ee6009f84fd361cf78f0185a3
Gustavo Ramírez <gustavo81988@gmail.com>
2018-08-07 15:33:33 -0500

    avances

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -2,22 +2,20 @@
 
 namespace App\Http\Controllers;
 use App\Quote;
+use App\Pdv;
 use Illuminate\Http\Request;
 
 class QuoteController extends Controller{
     public function postQuote(Request $request){
-        $quote = new Quote();
+        $quote = new Pdv();
         $quote->content = $request->input('content');
         $quote->save();
         return response()->json( ['quote' => $quote], 201 );
     }
 
     public function getQuote(){
-        $quotes = Quote::all();
-        $response = [
-            'quotes' => $quotes
-        ];
-        return response()->json($response, 200);
+        $ingredients = Pdv::all();
+        return response()->json($ingredients, 200);
     }
 
     public function putQuote(Request $request, $id){
